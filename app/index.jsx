38ccb1f9b599b871f5eaa3d6fdae72376218b9f5
Chris Williams <cchriswilliams721@gmail.com>
2024-06-07 18:04:37 -0600

    LoginPage in Development

---
+++ app/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.create}>Create an Account</Text>
+      </View>
+      <View style={styles.signMessage}>
+        <Text>Enter your email to sign up for this app</Text>
+      </View>
+      <TextInput style={styles.input} placeholder='email@domain.com'/>
+      <View style={styles.signInBtn}>
+      <Button title='Sign up with email' />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  create: {
+    fontWeight: 'bold',
+    fontSize: 25,
+
+  },
+  input:{
+    borderWidth: 2,
+    textDecorationLine: 'none',
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 327,
+    height:40,
+    borderRadius: 5,
+  },
+  signInBtn:{
+    backgroundColor: '#085F63',
+    width: 327,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  }
+});
